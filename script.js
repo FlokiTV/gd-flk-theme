11221e81a4434e0fcca6b9142c7dfd00a7eb5653
Floki <dawysonw@gmail.com>
2023-03-18 13:15:16 -0300

    update

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+
+
+let lastTime = 0;
+function animate(timestamp) {
+  const deltaTime = timestamp - lastTime;
+  lastTime = timestamp;
+  loop(deltaTime)
+  requestAnimationFrame(animate);
+}
+
+requestAnimationFrame(animate);
+
 let mosaics = document.querySelectorAll(".mosaic-tile");
 let canvasEditor = null;
 
 for (let i = 0; i < mosaics.length; i++) {
   const element = mosaics[i];
-  console.log(element.querySelector("canvas"));
+  let editor = element.querySelector("canvas")
+  if(editor){
+    canvasEditor = editor;
+  }
+}
+
+const loop = delta =>{
+  console.log(delta)
 }
